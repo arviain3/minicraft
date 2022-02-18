b510ad9af0c438c163f2101bcde59be5683df91c
arviain3 <99014052+arviain3@users.noreply.github.com>
2022-02-18 14:52:47 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -144,3 +144,51 @@ function my_keydown(e)
         console.log("c");
     }
 }
+
+function up()
+{
+    if(player_y >=0)
+    {
+        player_y = player_y - block_image_height;
+        console.log("block image height = " +block_image_height);
+        console.log("when up arrow key is pressed, X = " + player_x +", Y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function down()
+{
+    if(player_y >=0)
+    {
+        player_y = player_y + block_image_height;
+        console.log("block image height = " +block_image_height);
+        console.log("when down arrow key is pressed, X = " + player_x +", Y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function left()
+{
+    if(player_y >0)
+    {
+        player_y = player_y + block_image_width;
+        console.log("block image width = " +block_image_width);
+        console.log("when left arrow key is pressed, X = " + player_x +", Y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
+
+function right()
+{
+    if(player_y <=850)
+    {
+        player_y = player_y + block_image_width;
+        console.log("block image width = " +block_image_width);
+        console.log("when right arrow key is pressed, X = " + player_x +", Y = "+player_y);
+        canvas.remove(player_object);
+        player_update();
+    }
+}
